db6de492764f4f70fdaedb591c947b338c016290
JonathanG321 <JonathanG321@ymail.com>
2024-03-11 11:04:45 -0400

    remove chart components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
 import { unstable_noStore as noStore } from "next/cache";
-import { useState } from "react";
-import type { GraphDataItem } from "~/utils/types";
+import Chart from "./_components/Chart";
 
 // import { CreatePost } from "~/app/_components/create-post";
 // import { api } from "~/trpc/server";
 
 export default async function Home() {
   noStore();
-  const data = useState<GraphDataItem[]>([
-    { date: new Date(), score: 4.7, reviewType: "payment", client: "me" },
-  ]);
-  const settings = useState({});
   // const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <Chart />
         {/* <CrudShowcase /> */}
       </div>
     </main>
