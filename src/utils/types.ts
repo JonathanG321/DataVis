71b880eb267641f1186a6cc65254cd08d1957958
JonathanG321 <JonathanG321@ymail.com>
2024-03-11 20:17:02 -0400

    add more types for filter test

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,17 @@
-export type GraphDataItem = {
-  date: string;
+export type BaseDataItem = {
   score: number;
   client: string;
   reviewType: "payment" | "general" | "history" | "scheduled";
+};
+
+export type RawDataItem = BaseDataItem & {
+  date: Date;
+};
+
+export type GraphDataItem = BaseDataItem & {
+  date: string;
+};
+
+export type FilterOptions = {
+  reviewType: ("payment" | "general" | "history" | "scheduled")[];
 };
