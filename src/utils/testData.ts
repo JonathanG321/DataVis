7496c8e89ce5ee9b9b382210f3aa840aa0f7f892
JonathanG321 <JonathanG321@ymail.com>
2024-03-12 14:19:44 -0400

    update test data

--- src/utils/testData.ts
+++ src/utils/testData.ts
@@ -1,64 +1,47 @@
-import type { RawDataItem } from "./types";
+import type { FilterOptions, RawDataItem } from "./types";
 
 export const testData: RawDataItem[] = [
-  {
-    date: new Date("2024-03-11T20:29:49.070Z"),
-    score: 3.774284838629044,
-    reviewType: "history",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-12T20:29:49.070Z"),
-    score: 2.8514437699426534,
-    reviewType: "scheduled",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-13T20:29:49.070Z"),
-    score: 4.517602832813586,
-    reviewType: "scheduled",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-14T20:29:49.070Z"),
-    score: 4.628286748722789,
-    reviewType: "payment",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-15T20:29:49.070Z"),
-    score: 2.3404726613547644,
-    reviewType: "history",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-16T20:29:49.070Z"),
-    score: 1.121220384712699,
-    reviewType: "general",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-17T20:29:49.070Z"),
-    score: 1.820234217920863,
-    reviewType: "history",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-18T20:29:49.070Z"),
-    score: 1.3061979678312474,
-    reviewType: "general",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-19T20:29:49.070Z"),
-    score: 1.4588094911924965,
-    reviewType: "general",
-    client: "me",
-  },
-  {
-    date: new Date("2024-03-20T20:29:49.070Z"),
-    score: 0.2508144485568997,
-    reviewType: "payment",
-    client: "me",
-  },
+  ...generateData(),
+  ...generateData(),
+  ...generateData(),
+  ...generateData(),
+  ...generateData(),
+  ...generateData(),
+  ...generateData(),
 ];
+
+export const baseData = testData.map((item) => {
+  const dateArr = item.date.toDateString().split(" ");
+  return {
+    ...item,
+    date: dateArr[1] + " " + dateArr[2],
+  };
+});
+
+export function generateData(): RawDataItem[] {
+  return Array.from({ length: 20 }).map((_, i) => {
+    const date = new Date();
+    date.setDate(date.getDate() + i);
+    let reviewType: FilterOptions["reviewType"][0] = "scheduled";
+    switch (Math.ceil(Math.random() * 4)) {
+      case 1:
+        reviewType = "general";
+        break;
+      case 2:
+        reviewType = "payment";
+        break;
+      case 3:
+        reviewType = "history";
+        break;
+      default:
+        reviewType = "scheduled";
+        break;
+    }
+    return {
+      date,
+      score: Math.round(Math.random() * 50) / 10,
+      reviewType,
+      client: "me",
+    };
+  });
+}
