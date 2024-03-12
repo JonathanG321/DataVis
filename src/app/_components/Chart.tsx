9981a3b25bf12d40ac5ce82781712b63ddffbf99
JonathanG321 <JonathanG321@ymail.com>
2024-03-11 20:52:07 -0400

    add basic data filter

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -1,32 +1,79 @@
 "use client";
 import { useState } from "react";
+import type { ChangeEvent } from "react";
+// import { useForm } from "react-hook-form";
 import { AgChartsReact } from "ag-charts-react";
 import type { AgChartProps } from "ag-charts-react";
-import type { GraphDataItem } from "~/utils/types";
+import type { FilterOptions, GraphDataItem } from "~/utils/types";
+import { testData } from "~/utils/testData";
+
+const baseData = testData.map((item) => {
+  const dateArr = item.date.toDateString().split(" ");
+  return {
+    ...item,
+    date: dateArr[1] + " " + dateArr[2],
+  };
+});
 
 export default function Chart() {
-  const [data, setData] = useState<GraphDataItem[]>(
-    Array.from({ length: 10 }).map((_, i) => {
-      const currentDate = new Date();
-      currentDate.setDate(currentDate.getDate() + i);
-      const dateArr = currentDate.toDateString().split(" ");
-      return {
-        date: dateArr[1] + " " + dateArr[2],
-        score: Math.random() * 5,
-        reviewType: "payment",
-        client: "me",
-      };
-    }),
-  );
-  const settings = useState({});
+  const [data, setData] = useState<GraphDataItem[]>(baseData);
+  const [reviewType, setReviewType] = useState<FilterOptions["reviewType"]>([]);
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   setValue,
+  //   getValues,
+  //   // formState: { errors },
+  // } = useForm<FilterOptions>({
+  //   defaultValues: {
+  //     reviewType: {
+  //       general: false,
+  //       history: false,
+  //       payment: false,
+  //       scheduled: false,
+  //     },
+  //   },
+  // });
+  function handleReviewTypeChange(event: ChangeEvent<HTMLInputElement>) {
+    const name = event.currentTarget.name as FilterOptions["reviewType"][0];
+    if (reviewType.includes(name)) {
+      setReviewType(reviewType.filter((type) => type !== name));
+    } else {
+      setReviewType([...reviewType, name]);
+    }
+  }
+
+  function onSubmit(settings: FilterOptions) {
+    setData(filterData(settings));
+  }
+
+  function filterData(settings: FilterOptions): GraphDataItem[] {
+    const newArr = baseData.filter((item) => {
+      let shouldKeep = true;
+      if (
+        !!settings.reviewType.length &&
+        !settings.reviewType.includes(item.reviewType)
+      ) {
+        shouldKeep = false;
+      }
+      return shouldKeep;
+    });
+    return newArr;
+  }
   // const hello = await api.post.hello.query({ text: "from tRPC" });
   const options: AgChartProps["options"] = {
     title: { text: "Test" },
     subtitle: { text: "Data from 2023" },
     data,
+    width: 1000,
+    height: 600,
     series: [{ type: "line", xKey: "date", yKey: "score", yName: "Score" }],
     axes: [
-      { type: "category", position: "bottom" },
+      {
+        type: "category",
+        position: "bottom",
+      },
       {
         type: "number",
         position: "left",
@@ -37,11 +84,53 @@ export default function Chart() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container z-50 flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <AgChartsReact options={options} />
-        {/* <CrudShowcase /> */}
-      </div>
-    </main>
+    <div className="flex w-full flex-col items-center justify-center">
+      <AgChartsReact options={options} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit({ reviewType });
+        }}
+      >
+        <label>
+          <input
+            type="checkbox"
+            checked={reviewType.includes("general")}
+            name="general"
+            onChange={handleReviewTypeChange}
+          />
+          General
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={reviewType.includes("history")}
+            name="history"
+            onChange={handleReviewTypeChange}
+          />
+          History
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={reviewType.includes("payment")}
+            name="payment"
+            onChange={handleReviewTypeChange}
+          />
+          Payment
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={reviewType.includes("scheduled")}
+            name="scheduled"
+            onChange={handleReviewTypeChange}
+          />
+          Scheduled
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {/* <CrudShowcase /> */}
+    </div>
   );
 }
