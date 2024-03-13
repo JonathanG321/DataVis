a81a976ffdab0deeba2af332785e3a5aafdddb8d
JonathanG321 <JonathanG321@ymail.com>
2024-03-13 14:28:16 -0400

    add time range filter and refactor

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -1,59 +1,62 @@
 "use client";
 import { useState } from "react";
-// import { useForm } from "react-hook-form";
-import { AgChartsReact } from "ag-charts-react";
-import type { AgChartProps } from "ag-charts-react";
+import { type AgChartProps, AgChartsReact } from "ag-charts-react";
 import type { FilterOptions, GraphData, DataItem } from "~/utils/types";
 import { baseData } from "~/utils/testData";
 import FilterSettings from "./FilterSettings";
 
-const baseCharOptions: Exclude<AgChartProps["options"], "data"> = {
+const baseChartOptions: Exclude<AgChartProps["options"], "data"> = {
   title: { text: "Test" },
-  subtitle: { text: "Data from 2023" },
   width: 1000,
   height: 600,
-  series: [
-    {
-      type: "line",
-      xKey: "dateLabel",
-      yKey: "filteredScore",
-      yName: "Filtered Score",
-    },
-    {
-      type: "line",
-      xKey: "dateLabel",
-      yKey: "totalScore",
-      yName: "Total Score",
-    },
-  ],
   theme: "ag-polychroma-dark",
+  subtitle: {
+    text: `Data from ${new Date().toDateString().split(" ")[1]} ${new Date().getFullYear()}`,
+  },
 };
-
+const simpleSeries: AgChartProps["options"]["series"] = [
+  {
+    type: "line",
+    xKey: "dateLabel",
+    yKey: "totalScore",
+    yName: "Score",
+  },
+];
+const filteredSeries: AgChartProps["options"]["series"] = [
+  {
+    type: "line",
+    xKey: "dateLabel",
+    yKey: "filteredScore",
+    yName: "Filtered Score",
+  },
+  {
+    type: "line",
+    xKey: "dateLabel",
+    yKey: "totalScore",
+    yName: "Total Score",
+  },
+];
 export default function Chart() {
-  const [data, setData] = useState<DataItem[]>(baseData);
+  const [filterOptions, setFilterOptions] = useState<FilterOptions>({
+    reviewType: [],
+    timeFrameYear: new Date().getFullYear(),
+    timeFrameMonth: new Date().toDateString().split(" ")[1] ?? "Jan",
+    timeFrameType: "monthly",
+  });
+  const [chartOptions, setChartOptions] =
+    useState<AgChartProps["options"]>(baseChartOptions);
+  const dateFilteredData = baseData.filter(
+    (item) =>
+      item.date.getFullYear() === filterOptions.timeFrameYear &&
+      item.date.toDateString().split(" ")[1] === filterOptions.timeFrameMonth,
+  );
+  const [totalDataState, setTotalDataState] =
+    useState<DataItem[]>(dateFilteredData);
   const [filteredDataState, setFilteredDataState] =
-    useState<DataItem[]>(baseData);
-  const [reviewType, setReviewType] = useState<FilterOptions["reviewType"]>([]);
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   setValue,
-  //   getValues,
-  //   // formState: { errors },
-  // } = useForm<FilterOptions>({
-  //   defaultValues: {
-  //     reviewType: {
-  //       general: false,
-  //       history: false,
-  //       payment: false,
-  //       scheduled: false,
-  //     },
-  //   },
-  // });
+    useState<DataItem[]>(dateFilteredData);
 
   // const hello = await api.post.hello.query({ text: "from tRPC" });
-  const totalDataObject = getDataObject(baseData);
+  const totalDataObject = getDataObject(totalDataState);
   const filteredDataObject = getDataObject(filteredDataState);
   const totalData: GraphData = Object.entries(totalDataObject).map(
     ([key, value]) => ({
@@ -62,21 +65,27 @@ export default function Chart() {
       dateLabel: key,
     }),
   );
-  const options: AgChartProps["options"] = {
-    ...baseCharOptions,
+  const options = {
+    ...chartOptions,
     data: totalData,
-  };
+    series:
+      totalDataState.length === filteredDataState.length
+        ? simpleSeries
+        : filteredSeries,
+  } as AgChartProps["options"];
 
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <AgChartsReact options={options} />
-      <FilterSettings
-        baseData={data}
-        setBaseData={setData}
-        setFilteredData={setFilteredDataState}
-        reviewType={reviewType}
-        setReviewType={setReviewType}
-      />
+      <div className="flex">
+        <AgChartsReact options={options} />
+        <FilterSettings
+          setChartOptions={setChartOptions}
+          setTotalData={setTotalDataState}
+          setFilteredData={setFilteredDataState}
+          filterOptions={filterOptions}
+          setFilterOptions={setFilterOptions}
+        />
+      </div>
       {/* <CrudShowcase /> */}
     </div>
   );
--- src/app/_components/FilterSettings.tsx
+++ src/app/_components/FilterSettings.tsx
@@ -1,32 +1,34 @@
-import {
-  useState,
-  type ChangeEvent,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
+import type { ChangeEvent, Dispatch, SetStateAction } from "react";
+import type { AgChartProps } from "ag-charts-react";
+import { updateStateObject } from "~/utils/helperFunctions";
+import { baseData } from "~/utils/testData";
 import type { FilterOptions, DataItem } from "~/utils/types";
 
 type Props = {
-  baseData: DataItem[];
-  setBaseData: Dispatch<SetStateAction<DataItem[]>>;
+  setTotalData: Dispatch<SetStateAction<DataItem[]>>;
   setFilteredData: Dispatch<SetStateAction<DataItem[]>>;
-  reviewType: FilterOptions["reviewType"];
-  setReviewType: Dispatch<SetStateAction<FilterOptions["reviewType"]>>;
+  filterOptions: FilterOptions;
+  setFilterOptions: Dispatch<SetStateAction<FilterOptions>>;
+  setChartOptions: Dispatch<SetStateAction<AgChartProps["options"]>>;
 };
 
 export default function FilterSettings({
-  baseData,
-  setBaseData,
+  setTotalData,
   setFilteredData,
-  reviewType,
-  setReviewType,
+  filterOptions,
+  setFilterOptions,
+  setChartOptions,
 }: Props) {
-  const [timeFrameYear, setTimeFrameYear] = useState(new Date().getFullYear());
-  const [timeFrameMonth, setTimeFrameMonth] = useState(
-    new Date().toDateString().split(" ")[1],
-  );
   function onSubmit(settings: FilterOptions) {
-    setFilteredData(filterData(settings));
+    setFilteredData(dateFilter(filterData(settings)));
+    setTotalData(dateFilter(baseData));
+    updateStateObject(
+      "subtitle",
+      {
+        text: `Data from ${settings.timeFrameType === "monthly" ? settings.timeFrameMonth + " " : ""}${settings.timeFrameYear}`,
+      },
+      setChartOptions,
+    );
   }
 
   function filterData(settings: FilterOptions): DataItem[] {
@@ -42,92 +44,142 @@ export default function FilterSettings({
     });
     return newArr;
   }
+  function dateFilter(data: DataItem[]) {
+    return data.filter((item) => {
+      const itemIsYear =
+        item.date.getFullYear() === filterOptions.timeFrameYear;
+      const itemIsMonth =
+        item.date.toDateString().split(" ")[1] === filterOptions.timeFrameMonth;
+      return filterOptions.timeFrameType === "monthly"
+        ? itemIsYear && itemIsMonth
+        : itemIsYear;
+    });
+  }
 
   function handleReviewTypeChange(event: ChangeEvent<HTMLInputElement>) {
     const name = event.currentTarget.name as FilterOptions["reviewType"][0];
-    if (reviewType.includes(name)) {
-      setReviewType(reviewType.filter((type) => type !== name));
-    } else {
-      setReviewType([...reviewType, name]);
-    }
+    const newReviewType = filterOptions.reviewType.includes(name)
+      ? filterOptions.reviewType.filter((type) => type !== name)
+      : [...filterOptions.reviewType, name];
+
+    updateStateObject("reviewType", newReviewType, setFilterOptions);
   }
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        onSubmit({ reviewType });
+        onSubmit(filterOptions);
       }}
     >
-      <label>
-        <input
-          type="checkbox"
-          checked={reviewType.includes("general")}
-          name="general"
-          onChange={handleReviewTypeChange}
-        />
-        General
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={reviewType.includes("history")}
-          name="history"
-          onChange={handleReviewTypeChange}
-        />
-        History
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={reviewType.includes("payment")}
-          name="payment"
-          onChange={handleReviewTypeChange}
-        />
-        Payment
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={reviewType.includes("scheduled")}
-          name="scheduled"
-          onChange={handleReviewTypeChange}
-        />
-        Scheduled
-      </label>
-      <label>
-        <select
-          className="text-black"
-          value={timeFrameYear}
-          onChange={(e) => setTimeFrameYear(parseInt(e.target.value))}
-        >
-          {Array.from({ length: 20 }).map((_, i) => (
-            <option key={2020 + i} value={2020 + i}>
-              {2020 + i}
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={filterOptions.reviewType.includes("general")}
+            name="general"
+            onChange={handleReviewTypeChange}
+          />
+          General
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={filterOptions.reviewType.includes("history")}
+            name="history"
+            onChange={handleReviewTypeChange}
+          />
+          History
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={filterOptions.reviewType.includes("payment")}
+            name="payment"
+            onChange={handleReviewTypeChange}
+          />
+          Payment
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={filterOptions.reviewType.includes("scheduled")}
+            name="scheduled"
+            onChange={handleReviewTypeChange}
+          />
+          Scheduled
+        </label>
+      </div>
+      <div>
+        <label>
+          Range
+          <select
+            className="text-black"
+            value={filterOptions.timeFrameType}
+            onChange={(e) =>
+              updateStateObject(
+                "timeFrameType",
+                e.target.value as FilterOptions["timeFrameType"],
+                setFilterOptions,
+              )
+            }
+          >
+            <option key={"monthly"} value={"monthly"}>
+              Monthly
             </option>
-          ))}
-        </select>
-        Year
-      </label>
-      <label>
-        <select
-          className="text-black"
-          value={timeFrameMonth}
-          onChange={(e) => setTimeFrameMonth(e.target.value)}
-        >
-          {Array.from({ length: 12 }).map((_, i) => {
-            const value = new Date(`${1 + i}/01/2024`)
-              .toDateString()
-              .split(" ")[1];
-            return (
-              <option key={value} value={value}>
-                {value}
+            <option key={"yearly"} value={"yearly"}>
+              Yearly
+            </option>
+          </select>
+        </label>
+        <label>
+          Year
+          <select
+            className="text-black"
+            value={filterOptions.timeFrameYear}
+            onChange={(e) =>
+              updateStateObject(
+                "timeFrameYear",
+                parseInt(e.target.value),
+                setFilterOptions,
+              )
+            }
+          >
+            {Array.from({ length: 20 }).map((_, i) => (
+              <option key={2020 + i} value={2020 + i}>
+                {2020 + i}
               </option>
-            );
-          })}
-        </select>
-        Month
-      </label>
+            ))}
+          </select>
+        </label>
+        {filterOptions.timeFrameType === "monthly" && (
+          <label>
+            Month
+            <select
+              className="text-black"
+              value={filterOptions.timeFrameMonth}
+              onChange={(e) =>
+                updateStateObject(
+                  "timeFrameMonth",
+                  e.target.value,
+                  setFilterOptions,
+                )
+              }
+            >
+              {Array.from({ length: 12 }).map((_, i) => {
+                const value = new Date(`${1 + i}/01/2024`)
+                  .toDateString()
+                  .split(" ")[1];
+                return (
+                  <option key={value} value={value}>
+                    {value}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+        )}
+      </div>
       <button type="submit">Submit</button>
     </form>
   );
