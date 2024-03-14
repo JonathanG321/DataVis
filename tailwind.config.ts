aab3d11850afed8ee2e368952e36199fea1b7fda
JonathanG321 <JonathanG321@ymail.com>
2024-03-13 20:48:02 -0400

    extend tailwind height options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      height: {
+        "200": "50rem",
+        "180": "45rem",
+        "160": "40rem",
+        "140": "35rem",
+        "120": "30rem",
+      },
     },
   },
   plugins: [],
