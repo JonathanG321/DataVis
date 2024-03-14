bdb668f86bb4223d37877f63ed26e948845d5ed6
JonathanG321 <JonathanG321@ymail.com>
2024-03-13 20:12:08 -0400

    add getWeek helper function

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -11,3 +11,12 @@ export function updateStateObject<K, T extends keyof K>(
 export function getMonth(date: Date) {
   return date.toDateString().split(" ")[1];
 }
+
+export function getWeek(date: Date) {
+  const newDate = new Date(date.toDateString());
+  const startDate = `${getMonth(newDate)} ${newDate.toDateString().split(" ")[2]}`;
+  newDate.setDate(newDate.getDate() + 7);
+  const endDate = `${getMonth(newDate)} ${newDate.toDateString().split(" ")[2]}`;
+  const weekDisplay = `${startDate}${date.getFullYear() !== newDate.getFullYear() ? " " + date.getFullYear() : ""} - ${endDate} ${newDate.getFullYear()}`;
+  return weekDisplay;
+}
