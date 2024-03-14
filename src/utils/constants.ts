3885d31137ad5e041f803b6b0db7d84ba866e803
JonathanG321 <JonathanG321@ymail.com>
2024-03-14 19:09:28 -0400

    add defaultFilters constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,14 @@ export const reviewTypes: FilterOptions["reviewType"] = [
   "history",
   "scheduled",
 ];
+
+const date = new Date();
+const weekDate = new Date();
+weekDate.setDate(weekDate.getDate() - weekDate.getDay());
+export const defaultFilters: FilterOptions = {
+  reviewType: [],
+  timeFrameYear: date.getFullYear(),
+  timeFrameMonth: date.getMonth(),
+  timeFrameWeek: weekDate,
+  timeFrameType: "monthly",
+};
