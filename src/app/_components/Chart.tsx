70556192fe80a6f2a2a914b448359116fb1abe42
JonathanG321 <JonathanG321@ymail.com>
2024-03-14 23:49:32 -0400

    refactor and add styling to chart

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -5,10 +5,12 @@ import type { GraphData, DataItem } from "~/utils/types";
 import { baseData } from "~/utils/testData";
 import FilterSettings from "./FilterSettings";
 import { defaultFilters } from "~/utils/constants";
+import DateRange from "./DateRange";
+import { getWeek } from "~/utils/helperFunctions";
 
 const baseChartOptions: Exclude<AgChartProps["options"], "data"> = {
-  title: { text: "Test" },
   background: { fill: "rgb(31 41 55)" },
+  padding: { bottom: 40, left: 40, right: 40, top: 40 },
   theme: {
     baseTheme: "ag-polychroma-dark",
     overrides: {
@@ -91,9 +93,16 @@ export default function Chart() {
       dateLabel: key,
     }),
   );
+  let footnote = filterOptions.timeFrameYear.toString();
+  if (filterOptions.timeFrameType === "monthly") {
+    footnote = filterOptions.timeFrameMonth + " " + footnote;
+  } else if (filterOptions.timeFrameType === "weekly") {
+    footnote = getWeek(filterOptions.timeFrameWeek);
+  }
   const options = {
     ...chartOptions,
     data: totalData,
+    footnote,
     series:
       totalDataState.length === filteredDataState.length
         ? simpleSeries
@@ -104,8 +113,30 @@ export default function Chart() {
     <div className="flex w-full flex-col items-center justify-center">
       <div className="flex w-full">
         <div className="w-9/12 p-2">
-          <div className="h-120 overflow-hidden rounded-lg border-2 border-gray-700 bg-gray-800">
-            <AgChartsReact options={options} />
+          <div className="flex flex-col rounded-lg border-2 border-gray-700 bg-gray-800">
+            <div className="flex justify-between border-b-2 border-gray-700">
+              <div className="flex">
+                <div className="my-2 flex w-32 flex-col items-center border-r-2 border-gray-700 p-2 px-4">
+                  <h5 className="text-center text-sm text-gray-500">
+                    Overall Score
+                  </h5>
+                </div>
+                <div className="my-2 w-32 border-r-2 border-gray-700 p-2 px-4">
+                  <h5 className="text-center text-sm text-gray-500">
+                    Filtered Score
+                  </h5>
+                </div>
+              </div>
+              <DateRange
+                filterOptions={filterOptions}
+                setFilterOptions={setFilterOptions}
+              />
+            </div>
+            <div className="h-120 flex justify-center overflow-hidden">
+              <div className="w-[98%]">
+                <AgChartsReact options={options} />
+              </div>
+            </div>
           </div>
         </div>
         <div className="w-3/12 p-2">
