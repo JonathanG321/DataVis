da2627a66d1137f7e9ae9314f4f00bda5979a073
JonathanG321 <JonathanG321@ymail.com>
2024-03-15 12:18:46 -0400

    refactor FilterSettings

--- src/app/_components/FilterSettings.tsx
+++ src/app/_components/FilterSettings.tsx
@@ -14,71 +14,25 @@ import { reviewTypes } from "~/utils/constants";
 import Accordion from "./Accordion";
 
 type Props = {
-  setTotalData: Dispatch<SetStateAction<DataItem[]>>;
-  setFilteredData: Dispatch<SetStateAction<DataItem[]>>;
   filterOptions: FilterOptions;
-  setFilterOptions: Dispatch<SetStateAction<FilterOptions>>;
-  setChartOptions: Dispatch<SetStateAction<AgChartProps["options"]>>;
+  setters: {
+    setTotalData: Dispatch<SetStateAction<DataItem[]>>;
+    setFilteredData: Dispatch<SetStateAction<DataItem[]>>;
+    setFilterOptions: Dispatch<SetStateAction<FilterOptions>>;
+    setChartOptions: Dispatch<SetStateAction<AgChartProps["options"]>>;
+  };
 };
 
-export default function FilterSettings({
-  setTotalData,
-  setFilteredData,
-  filterOptions,
-  setFilterOptions,
-  setChartOptions,
-}: Props) {
-  function onSubmit(e: FormEvent<HTMLFormElement>, filters: FilterOptions) {
-    e.preventDefault();
-    handleFilter(filters);
-  }
+type Setters = {
+  setFilterOptions: Dispatch<SetStateAction<FilterOptions>>;
+  setChartOptions: Dispatch<SetStateAction<AgChartProps["options"]>>;
+  setTotalData: Dispatch<SetStateAction<DataItem[]>>;
+  setFilteredData: Dispatch<SetStateAction<DataItem[]>>;
+};
 
-  function handleFilter(filters: FilterOptions) {
-    let newFilters = { ...filters };
-    let subtitle = newFilters.timeFrameYear.toString();
-    if (newFilters.timeFrameType === "monthly") {
-      subtitle = `${getMonth(new Date(`${newFilters.timeFrameMonth + 1}/01/2024`)) + " " + newFilters.timeFrameYear}`;
-    } else if (newFilters.timeFrameType === "weekly") {
-      newFilters = {
-        ...filters,
-        ...updateStateObject(
-          "timeFrameWeek",
-          filterOptions.timeFrameWeek,
-          setFilterOptions,
-        ),
-      };
-      subtitle = getWeek(newFilters.timeFrameWeek);
-    }
-    updateStateObject(
-      "subtitle",
-      { text: `Data from ${subtitle}` },
-      setChartOptions,
-    );
-    const newFilteredData = dateFilter(filterData(newFilters), newFilters);
-    const newTotalData = dateFilter(baseData, newFilters);
-    setFilteredData(newFilteredData);
-    setTotalData(newTotalData);
-  }
-
-  function resetFilters(
-    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
-  ) {
-    e.preventDefault();
-    setFilterOptions({ ...filterOptions, reviewType: [] });
-    handleFilter({ ...filterOptions, reviewType: [] });
-  }
-
-  function handleReviewTypeChange(event: ChangeEvent<HTMLInputElement>) {
-    const name = event.currentTarget.name as FilterOptions["reviewType"][0];
-    const newReviewType = filterOptions.reviewType.includes(name)
-      ? filterOptions.reviewType.filter((type) => type !== name)
-      : [...filterOptions.reviewType, name];
-
-    updateStateObject("reviewType", newReviewType, setFilterOptions);
-  }
-
+export default function FilterSettings({ filterOptions, setters }: Props) {
   useEffect(() => {
-    handleFilter(filterOptions);
+    handleFilter(filterOptions, setters);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     filterOptions.timeFrameType,
@@ -90,7 +44,7 @@ export default function FilterSettings({
   return (
     <form
       className="h-full rounded-lg border-2 border-gray-700 bg-gray-800"
-      onSubmit={(e) => onSubmit(e, filterOptions)}
+      onSubmit={(e) => onSubmit(e, filterOptions, setters)}
     >
       <div className="flex justify-between p-4">
         <h3 className="text-2xl font-bold">Filters</h3>
@@ -98,12 +52,12 @@ export default function FilterSettings({
           <button
             className="mr-2 rounded border border-gray-600 bg-gray-700 px-2 text-sm font-bold text-gray-500"
             type="button"
-            onClick={resetFilters}
+            onClick={(e) => resetFilters(e, filterOptions, setters)}
           >
             Reset
           </button>
           <button
-            className="rounded border border-yellow-600 bg-yellow-700 px-2 text-sm font-bold text-yellow-600"
+            className="rounded border border-yellow-600 bg-yellow-800 px-2 text-sm font-bold text-yellow-600"
             type="submit"
           >
             Apply
@@ -117,7 +71,13 @@ export default function FilterSettings({
               type="checkbox"
               checked={filterOptions.reviewType.includes(type)}
               name={type}
-              onChange={handleReviewTypeChange}
+              onChange={(e) =>
+                handleReviewTypeChange(
+                  e,
+                  filterOptions,
+                  setters.setFilterOptions,
+                )
+              }
             />
             <span className="ml-2">
               {type.replace(type[0] ?? "", (type[0] ?? "").toUpperCase())}
@@ -168,4 +128,63 @@ function filterData(settings: FilterOptions): DataItem[] {
     return shouldKeep;
   });
   return newArr;
+}
+
+function onSubmit(
+  e: FormEvent<HTMLFormElement>,
+  filters: FilterOptions,
+  setters: Setters,
+) {
+  e.preventDefault();
+  handleFilter(filters, setters);
+}
+
+function handleFilter(filters: FilterOptions, setters: Setters) {
+  let newFilters = { ...filters };
+  let subtitle = newFilters.timeFrameYear.toString();
+  if (newFilters.timeFrameType === "monthly") {
+    subtitle = `${getMonth(new Date(`${newFilters.timeFrameMonth + 1}/01/2024`)) + " " + newFilters.timeFrameYear}`;
+  } else if (newFilters.timeFrameType === "weekly") {
+    newFilters = {
+      ...filters,
+      ...updateStateObject(
+        "timeFrameWeek",
+        filters.timeFrameWeek,
+        setters.setFilterOptions,
+      ),
+    };
+    subtitle = getWeek(newFilters.timeFrameWeek);
+  }
+  updateStateObject(
+    "subtitle",
+    { text: `Data from ${subtitle}` },
+    setters.setChartOptions,
+  );
+  const newFilteredData = dateFilter(filterData(newFilters), newFilters);
+  const newTotalData = dateFilter(baseData, newFilters);
+  setters.setFilteredData(newFilteredData);
+  setters.setTotalData(newTotalData);
+}
+
+function handleReviewTypeChange(
+  event: ChangeEvent<HTMLInputElement>,
+  filterOptions: FilterOptions,
+  setFilterOptions: Setters["setFilterOptions"],
+) {
+  const name = event.currentTarget.name as FilterOptions["reviewType"][0];
+  const newReviewType = filterOptions.reviewType.includes(name)
+    ? filterOptions.reviewType.filter((type) => type !== name)
+    : [...filterOptions.reviewType, name];
+
+  updateStateObject("reviewType", newReviewType, setFilterOptions);
+}
+
+function resetFilters(
+  e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+  filterOptions: FilterOptions,
+  setters: Setters,
+) {
+  e.preventDefault();
+  setters.setFilterOptions({ ...filterOptions, reviewType: [] });
+  handleFilter({ ...filterOptions, reviewType: [] }, setters);
 }
