1e0bc83f43ff0ad306f7c9bcc87e3134923f35db
JonathanG321 <JonathanG321@ymail.com>
2024-03-15 12:19:03 -0400

    style DateRange

--- src/app/_components/DateRange.tsx
+++ src/app/_components/DateRange.tsx
@@ -14,12 +14,12 @@ export default function DateRange({ setFilterOptions, filterOptions }: Props) {
     newWeek.setDate(newWeek.getDate() + dateChange);
     setFilterOptions({ ...filterOptions, timeFrameWeek: newWeek });
   }
-  const selectClasses = "rounded bg-gray-700 py-1 pl-2 mr-2";
+  const selectClasses = "rounded bg-gray-700 py-1 pl-2 ml-2";
   return (
-    <div className="my-2 w-96 border-l-2 border-gray-700 p-2 px-4">
-      <h5 className="text-sm text-gray-500">Timeframe</h5>
+    <div className="my-2 w-72 border-l-2 border-gray-700 p-2 px-4">
       <div className="flex justify-between">
-        <div className="flex justify-between py-2">
+        <h5 className="text-sm text-gray-500">Timeframe</h5>
+        <div className="flex justify-between">
           <select
             className={selectClasses}
             value={filterOptions.timeFrameType}
@@ -38,77 +38,77 @@ export default function DateRange({ setFilterOptions, filterOptions }: Props) {
             ))}
           </select>
         </div>
-        <div className="flex">
-          {filterOptions.timeFrameType === "monthly" && (
-            <div className="flex justify-between py-2">
-              <select
-                className={selectClasses}
-                value={filterOptions.timeFrameMonth}
-                onChange={(e) =>
-                  updateStateObject(
-                    "timeFrameMonth",
-                    parseInt(e.target.value),
-                    setFilterOptions,
-                  )
-                }
-              >
-                {Array.from({ length: 12 }).map((_, i) => {
-                  const date = new Date(`${1 + i}/01/2024`)
-                    .toDateString()
-                    .split(" ")[1];
-                  return (
-                    <option key={date} value={i}>
-                      {date}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-          )}
-          {(filterOptions.timeFrameType === "yearly" ||
-            filterOptions.timeFrameType === "monthly") && (
-            <div className="flex justify-between py-2">
-              <select
-                className={selectClasses}
-                value={filterOptions.timeFrameYear}
-                onChange={(e) =>
-                  updateStateObject(
-                    "timeFrameYear",
-                    parseInt(e.target.value),
-                    setFilterOptions,
-                  )
-                }
-              >
-                {Array.from({ length: 20 }).map((_, i) => (
-                  <option key={2020 + i} value={2020 + i}>
-                    {2020 + i}
+      </div>
+      <div className="flex justify-end">
+        {filterOptions.timeFrameType === "monthly" && (
+          <div className="flex justify-between py-2">
+            <select
+              className={selectClasses}
+              value={filterOptions.timeFrameMonth}
+              onChange={(e) =>
+                updateStateObject(
+                  "timeFrameMonth",
+                  parseInt(e.target.value),
+                  setFilterOptions,
+                )
+              }
+            >
+              {Array.from({ length: 12 }).map((_, i) => {
+                const date = new Date(`${1 + i}/01/2024`)
+                  .toDateString()
+                  .split(" ")[1];
+                return (
+                  <option key={date} value={i}>
+                    {date}
                   </option>
-                ))}
-              </select>
-            </div>
-          )}
-          {filterOptions.timeFrameType === "weekly" && (
-            <div className="flex items-center justify-between py-2">
-              <button
-                className="mr-2 rounded border border-yellow-600 bg-yellow-700 px-2 py-1 text-yellow-600"
-                type="button"
-                onClick={() => setWeekly(-7)}
-              >
-                {"<"}
-              </button>
-              <span className="text-xs">
-                {getWeek(filterOptions.timeFrameWeek)}
-              </span>
-              <button
-                className="ml-2 rounded border border-yellow-600 bg-yellow-700 px-2 py-1 text-yellow-600"
-                type="button"
-                onClick={() => setWeekly(7)}
-              >
-                {">"}
-              </button>
-            </div>
-          )}
-        </div>
+                );
+              })}
+            </select>
+          </div>
+        )}
+        {(filterOptions.timeFrameType === "yearly" ||
+          filterOptions.timeFrameType === "monthly") && (
+          <div className="flex justify-between py-2">
+            <select
+              className={selectClasses}
+              value={filterOptions.timeFrameYear}
+              onChange={(e) =>
+                updateStateObject(
+                  "timeFrameYear",
+                  parseInt(e.target.value),
+                  setFilterOptions,
+                )
+              }
+            >
+              {Array.from({ length: 20 }).map((_, i) => (
+                <option key={2020 + i} value={2020 + i}>
+                  {2020 + i}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+        {filterOptions.timeFrameType === "weekly" && (
+          <div className="flex items-center justify-between py-2">
+            <button
+              className="mr-2 rounded border border-yellow-600 bg-yellow-800 px-2 py-1 text-yellow-600"
+              type="button"
+              onClick={() => setWeekly(-7)}
+            >
+              {"<"}
+            </button>
+            <span className="text-xs">
+              {getWeek(filterOptions.timeFrameWeek)}
+            </span>
+            <button
+              className="ml-2 rounded border border-yellow-600 bg-yellow-800 px-2 py-1 text-yellow-600"
+              type="button"
+              onClick={() => setWeekly(7)}
+            >
+              {">"}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
