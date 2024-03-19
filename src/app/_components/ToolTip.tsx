44273d8bba3911620dcb50d37ca17c363318c10f
JonathanG321 <JonathanG321@ymail.com>
2024-03-19 15:59:42 -0400

    add responses to tooltips

--- src/app/_components/ToolTip.tsx
+++ src/app/_components/ToolTip.tsx
@@ -10,20 +10,30 @@ export function ToolTip(params: AgLineSeriesTooltipRendererParams<GraphData>) {
   const title = titleArr.join(" ");
   const totalScore = datum.totalScore?.toFixed(1);
   const filteredScore = datum.filteredScore?.toFixed(1);
+  const totalResponses = datum.totalResponses;
+  const filteredResponses = datum.filteredResponses;
   const isFiltered = totalScore !== filteredScore;
   return (
     <div className="rounded border-2 border-gray-700 bg-gray-800 p-3">
       <h3 className="mb-2 font-bold text-white">{title}</h3>
       <div className="flex flex-col">
-        <div className={classNames("flex", { "mb-2": isFiltered })}>
-          <span className="rounded bg-green-900 px-2 text-lg font-bold text-green-500">
+        <div
+          className={classNames("flex items-center", { "mb-2": isFiltered })}
+        >
+          <span className="min-w-12 rounded bg-green-900 px-2 text-center text-lg font-bold text-green-500">
             {totalScore}
+          </span>
+          <span className="px-2 text-sm text-gray-500">
+            {totalResponses} {isFiltered ? "Total" : ""} Responses
           </span>
         </div>
         {isFiltered && (
-          <div className="flex">
-            <span className="rounded bg-yellow-800 px-2 text-lg font-bold text-yellow-600">
-              {filteredScore}
+          <div className="flex items-center">
+            <span className="min-w-12 rounded bg-yellow-800 px-2 text-center text-lg font-bold text-yellow-600">
+              {filteredResponses}
+            </span>
+            <span className="px-2 text-sm text-gray-500">
+              {filteredResponses} Filtered Responses
             </span>
           </div>
         )}
