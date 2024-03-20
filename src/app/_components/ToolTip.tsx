c475aaf7eba22ef35ff736803ae491ff8d56d722
JonathanG321 <JonathanG321@ymail.com>
2024-03-20 14:45:40 -0400

    fix tooltip to show proper title based on timeFrameType

--- src/app/_components/ToolTip.tsx
+++ src/app/_components/ToolTip.tsx
@@ -1,18 +1,29 @@
 import type { AgLineSeriesTooltipRendererParams } from "ag-charts-community";
 import classNames from "classnames";
-import type { GraphData } from "~/utils/types";
+import type { FilterOptions, GraphData } from "~/utils/types";
 
-export function ToolTip(params: AgLineSeriesTooltipRendererParams<GraphData>) {
+type Props = {
+  params: AgLineSeriesTooltipRendererParams<GraphData>;
+  timeFrameType: FilterOptions["timeFrameType"];
+};
+
+export function ToolTip({ params, timeFrameType }: Props) {
   const { datum } = params;
-  const titleArr = datum.date.toDateString().split(" ");
-  titleArr.shift();
-  titleArr[1] = titleArr[1] + ",";
-  const title = titleArr.join(" ");
+  const dateString = datum.date.toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+  let title = dateString;
+  const titleArr = dateString.split(" ");
+  if (timeFrameType === "yearly") title = titleArr[0] || "";
   const totalScore = datum.totalScore?.toFixed(1);
   const filteredScore = datum.filteredScore?.toFixed(1);
   const totalResponses = datum.totalResponses;
   const filteredResponses = datum.filteredResponses;
-  const isFiltered = totalScore !== filteredScore;
+  const isFiltered =
+    totalScore !== filteredScore ||
+    datum.totalResponses !== datum.filteredResponses;
   return (
     <div className="rounded border-2 border-gray-700 bg-gray-800 p-3">
       <h3 className="mb-2 font-bold text-white">{title}</h3>
@@ -30,7 +41,7 @@ export function ToolTip(params: AgLineSeriesTooltipRendererParams<GraphData>) {
         {isFiltered && (
           <div className="flex items-center">
             <span className="min-w-12 rounded bg-yellow-800 px-2 text-center text-lg font-bold text-yellow-600">
-              {filteredResponses}
+              {filteredScore ?? "N/A"}
             </span>
             <span className="px-2 text-sm text-gray-500">
               {filteredResponses} Filtered Responses
