59a9fc13dc2948f24c9422fe2475d878c80a7834
JonathanG321 <JonathanG321@ymail.com>
2024-03-21 13:34:15 -0400

    refactor Chart component to fix yearly time frame long load time

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { type AgChartProps, AgChartsReact } from "ag-charts-react";
-import type { GraphData, DataItem } from "~/utils/types";
+import type { GraphData, DataItem, FilterOptions } from "~/utils/types";
 import { baseData } from "~/utils/testData";
 import FilterSettings from "./FilterSettings";
 import { defaultFilters } from "~/utils/constants";
@@ -10,6 +10,7 @@ import ChartHeader from "./ChartHeader";
 import { renderToString } from "react-dom/server";
 import { ToolTip } from "./ToolTip";
 import type { AgLineSeriesTooltipRendererParams } from "ag-charts-community";
+import ChartDisplay from "./ChartDisplay";
 
 const baseChartOptions: Exclude<AgChartProps["options"], "data"> = {
   background: { fill: "rgb(31 41 55)" },
@@ -59,26 +60,29 @@ export default function Chart() {
   const [filteredDataState, setFilteredDataState] =
     useState<DataItem[]>(dateFilteredData);
 
-  // const hello = await api.post.hello.query({ text: "from tRPC" });
-  const totalDataObject = getDataObject(totalDataState);
-  const filteredDataObject = getDataObject(filteredDataState);
+  const totalDataObject = getDataObject(totalDataState, timeFrameType);
+  const filteredDataObject = getDataObject(filteredDataState, timeFrameType);
   const totalData: GraphData[] = Object.entries(totalDataObject).map(
     ([key, value]) => {
+      const dateKey =
+        timeFrameType === "yearly"
+          ? new Date(timeFrameYear, parseInt(key))
+          : new Date(key);
       return {
         totalScore: parseFloat(value.toFixed(1)),
         filteredScore: parseFloat((filteredDataObject[key] ?? 0).toFixed(1)),
-        date: new Date(key),
+        date: dateKey,
         dateLabel: key,
         filteredResponses: filteredDataState.reduce(
           (total, current) =>
-            current.date.toDateString() === new Date(key).toDateString()
+            current.date.toDateString() === dateKey.toDateString()
               ? total + 1
               : total,
           0,
         ),
         totalResponses: totalDataState.reduce(
           (total, current) =>
-            current.date.toDateString() === new Date(key).toDateString()
+            current.date.toDateString() === dateKey.toDateString()
               ? total + 1
               : total,
           0,
@@ -87,199 +91,7 @@ export default function Chart() {
     },
   );
 
-  let footnote = timeFrameYear.toString();
-  let data: GraphData[] = [];
-  if (timeFrameType === "monthly") {
-    footnote =
-      getMonth(new Date(timeFrameMonth + 1 + "/01/2024")) + " " + footnote;
-    data = Array.from({
-      length: new Date(timeFrameYear, timeFrameMonth + 1, 0).getDate(),
-    }).map((_, i): GraphData => {
-      const date = new Date(timeFrameYear, timeFrameMonth, i + 1);
-      const item = totalData.find(
-        (data) => data.date.toDateString() === date.toDateString(),
-      );
-      if (item) return item;
-      return {
-        date,
-        dateLabel: date.toDateString(),
-      };
-    });
-  } else if (timeFrameType === "weekly") {
-    footnote = getWeek(timeFrameWeek);
-    data = Array.from({
-      length: 7,
-    }).map((_, i) => {
-      const weekDate = new Date(
-        timeFrameWeek.getFullYear(),
-        timeFrameWeek.getMonth(),
-        timeFrameWeek.getDate() + i,
-      );
-      const item = totalData.find(
-        (data) => data.date.toDateString() === weekDate.toDateString(),
-      );
-      if (item) return item;
-      return {
-        date: weekDate,
-        dateLabel: weekDate.toDateString(),
-      };
-    });
-  } else if (timeFrameType === "yearly") {
-    data = Array.from({
-      length: 12,
-    }).map((_, i) => {
-      const date = new Date(timeFrameYear, i, 1);
-      const items = totalData.filter(
-        (item) => item.date.getMonth() === date.getMonth(),
-      );
-      return items.reduce(
-        (finalItem, currentItem) => {
-          return {
-            ...finalItem,
-            totalScore: finalItem.totalScore
-              ? (finalItem.totalScore +
-                  (currentItem.totalScore ?? finalItem.totalScore)) /
-                2
-              : currentItem.totalScore,
-            filteredResponses:
-              (finalItem.filteredResponses || 0) +
-              (currentItem.filteredResponses || 0),
-            filteredScore: finalItem.filteredScore
-              ? (finalItem.filteredScore +
-                  (currentItem.filteredScore ?? finalItem.filteredScore)) /
-                2
-              : currentItem.filteredScore,
-            totalResponses:
-              (finalItem.totalResponses || 0) +
-              (currentItem.totalResponses || 0),
-          };
-        },
-        {
-          date,
-          dateLabel: date.toLocaleDateString("en-US", { month: "long" }),
-          totalScore: undefined,
-          filteredResponses: 0,
-          filteredScore: undefined,
-          totalResponses: 0,
-        },
-      );
-    });
-  }
-
-  const tooltip = {
-    enabled: true,
-    renderer: (params: AgLineSeriesTooltipRendererParams<GraphData>) =>
-      renderToString(<ToolTip params={params} timeFrameType={timeFrameType} />),
-  };
-
-  const options = {
-    ...chartOptions,
-    data,
-    axes: [
-      {
-        type: "category",
-        position: "bottom",
-        title: { enabled: true, text: footnote, color: "gray" },
-        label: {
-          enabled: true,
-          formatter: ({ value }) => {
-            const valArr = (value as string).split(" ");
-            if (timeFrameType === "monthly") {
-              return parseInt(valArr[2] ?? "").toString();
-            } else if (timeFrameType === "weekly") {
-              return (
-                valArr[0] + ", " + valArr[1] + " " + parseInt(valArr[2] ?? "")
-              );
-            } else if (timeFrameType === "yearly") {
-              return valArr[1];
-            }
-          },
-        },
-        tick: { enabled: true },
-        keys: ["dateLabel"],
-      },
-      {
-        type: "number",
-        position: "right",
-        keys: ["totalScore", "filteredScore"],
-        max: 5,
-        min: 0,
-      },
-      {
-        type: "number",
-        position: "left",
-        keys: ["totalResponses", "filteredResponses"],
-      },
-    ],
-    series: [
-      ...(totalDataState.length !== filteredDataState.length
-        ? [
-            {
-              type: "bar",
-              xKey: "dateLabel",
-              yKey: "filteredResponses",
-              yName: "Filtered Responses",
-              tooltip,
-              grouped: true,
-              fill: "gray",
-              cornerRadius: 2,
-            },
-          ]
-        : []),
-      {
-        type: "bar",
-        xKey: "dateLabel",
-        yKey: "totalResponses",
-        yName:
-          totalDataState.length === filteredDataState.length
-            ? "Responses"
-            : "Total Responses",
-        tooltip,
-        grouped: true,
-        fill: "darkGray",
-        cornerRadius: 2,
-      },
-      ...(totalDataState.length !== filteredDataState.length
-        ? [
-            {
-              type: "line",
-              xKey: "dateLabel",
-              yKey: "filteredScore",
-              yName: "Filtered Score",
-              tooltip,
-              connectMissingData: true,
-              marker: {
-                enabled: true,
-                fill: "rgb(202 138 4)",
-                stroke: "rgb(202 138 4)",
-                strokeOpacity: 0.5,
-                strokeWidth: 6,
-              },
-              stroke: "rgb(202 138 4)",
-            },
-          ]
-        : []),
-      {
-        type: "line",
-        xKey: "dateLabel",
-        yKey: "totalScore",
-        yName:
-          totalDataState.length === filteredDataState.length
-            ? "Score"
-            : "Total Score",
-        tooltip,
-        connectMissingData: true,
-        marker: {
-          enabled: true,
-          fill: "rgb(34 197 94)",
-          stroke: "rgb(34 197 94)",
-          strokeOpacity: 0.5,
-          strokeWidth: 6,
-        },
-        stroke: "rgb(34 197 94)",
-      },
-    ],
-  } as AgChartProps["options"];
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
 
   const setters = {
     setChartOptions,
@@ -298,11 +110,13 @@ export default function Chart() {
               setFilterOptions={setFilterOptions}
               totalData={totalData}
             />
-            <div className="h-120 flex justify-center overflow-hidden">
-              <div className="mb-2 w-full">
-                <AgChartsReact options={options} />
-              </div>
-            </div>
+            <ChartDisplay
+              filteredDataState={filteredDataState}
+              totalDataState={totalDataState}
+              totalData={totalData}
+              chartOptions={chartOptions}
+              filterOptions={filterOptions}
+            />
           </div>
         </div>
         <div className="w-3/12 p-4 pl-2">
@@ -314,14 +128,17 @@ export default function Chart() {
   );
 }
 
-function getDataObject(data: DataItem[]) {
+function getDataObject(
+  data: DataItem[],
+  timeFrameType: FilterOptions["timeFrameType"],
+) {
   return data.reduce(
     (object, { score, date }) => {
+      const key =
+        timeFrameType === "yearly" ? date.getMonth() : date.toDateString();
       return {
         ...object,
-        [date.toDateString()]: !!object[date.toDateString()]
-          ? (score + (object[date.toDateString()] ?? 0)) / 2
-          : score,
+        [key]: !!object[key] ? (score + (object[key] ?? 0)) / 2 : score,
       };
     },
     {} as Record<string, number>,
